293663974835aa9c84083f0b4acab3bcf954c660
Scott McKeon <scott.mckeon@chicagobooth.edu>
2022-04-28 17:41:22 +0000

    Currency selection page complete

--- app/controllers/exchange_controller.rb
+++ app/controllers/exchange_controller.rb
@@ -1,8 +1,15 @@
 class ExchangeController < ApplicationController
 
   def pick_currency
-    render({:template => "exchange_templates/pick.html.erb"})
+    exchange_raw_string = open("https://api.exchangerate.host/symbols").read
+    exchange_JSON = JSON.parse(exchange_raw_string)
+    key_data = exchange_JSON.fetch("symbols")
+    
 
+    @array_of_currencies = []
+    @array_of_currencies = key_data.keys
+
+    render({:template => "exchange_templates/pick.html.erb"})
   end
 
   def pairings
